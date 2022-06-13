efe700ae753608b18b17600752bcc3788030a364
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-06-13 23:36:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import winsound
-import platform
-import subprocess
-import requests
 
+import requests
 
 # duration = 3000
 # freq = 500
 # winsound.Beep(freq, duration)
 
 
-urls = ["http://www.google.com", "http://www.yandex.com"]
-timeout = 5
+def is_internet_available():
+    urls = "http://www.google.com"
+    timeout = 5
 
-for url in urls:
+    # for url in urls:
     try:
-        request = requests.get(url, timeout=timeout)
+        request = requests.get(urls, timeout=timeout)
         print("Connected to the Internet")
+        return True
     except (requests.ConnectionError, requests.Timeout) as exception:
         print("No internet connection.")
+        print(exception)
+        return False
+
+
+print(is_internet_available())
