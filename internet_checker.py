60808079326862c0e8aa203720b760b8976cc7eb
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-06-14 00:49:38 +0300

    Add retries

--- internet_checker.py
+++ internet_checker.py
@@ -3,8 +3,10 @@ import sys
 import winsound
 
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
-url = "http://www.google.com"
+url = "https://www.google.com"
 timeout = 5
 
 frequency = 500
@@ -23,12 +25,20 @@ def sound_notification(frequency: int, duration: int):
 
 def is_internet_available(url: str, timeout: int) -> bool:
     # for url in urls:
+    retry_strategy = Retry(total=100, status_forcelist=[429, 500, 502, 503, 504])
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    http = requests.Session()
+    http.mount("http://", adapter)
+    http.mount("https://", adapter)
     try:
-        request = requests.get(url, timeout=timeout)
-        print("Connected to the Internet")
-        return True
+        response = http.get(url, timeout=timeout)
+        if response.status_code == 200:
+            print("Connected to the Internet")
+            return True
+        else:
+            return False
     except (requests.ConnectionError, requests.Timeout) as exception:
-        print("No internet connection.")
+        print("No Internet connection")
         print(exception)
         return False
 
