e044af3e36ccdeecb6f5e03e6fa22f9ffa1fe4a2
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-06-14 02:18:35 +0300

    Update internet_checker.py

--- internet_checker.py
+++ internet_checker.py
@@ -4,8 +4,10 @@ import time
 import winsound
 
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
-# Arg parse for param ?
+# Arg parse for param
 # Use URL from args & file
 url = "https://www.google.com"
 timeout = 5
@@ -19,16 +21,22 @@ def sound_notification(frequency: int, duration: int):
         winsound.Beep(frequency, duration)
     else:
         try:
-            os.system("beep -f %s -l %s" % (frequency, duration))
+            # os.system("beep -f %s -l %s" % (frequency, duration))
+            print("Test")
         except OSError:
             print("Try to install beep to your system")
 
 
 def is_internet_available(url: str, timeout: int) -> bool:
     # for url in urls:
+    retry_strategy = Retry(total=100, status_forcelist=[429, 500, 502, 503, 504])
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    http = requests.Session()
+    http.mount("http://", adapter)
+    http.mount("https://", adapter)
     try:
-        request = requests.get(url, timeout=timeout)
-        if request.status_code == 200:
+        response = http.get(url, timeout=timeout)
+        if response.status_code == 200:
             return True
         else:
             return False
@@ -45,6 +53,7 @@ def is_internet_available(url: str, timeout: int) -> bool:
 if __name__ == "__main__":
     if is_internet_available(url, timeout):
         print("Connected to the Internet")
+        # sys.exit(0)
     else:
         print("No Internet connection")
         sound_notification(frequency, duration)
