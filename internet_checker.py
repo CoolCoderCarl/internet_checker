93410509dfe69dbd69cfb4547a842159ad145afe
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-06-14 02:10:41 +0300

    Update internet_checker.py

--- internet_checker.py
+++ internet_checker.py
@@ -4,10 +4,8 @@ import time
 import winsound
 
 import requests
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
 
-# Arg parse for param
+# Arg parse for param ?
 # Use URL from args & file
 url = "https://www.google.com"
 timeout = 5
@@ -28,14 +26,9 @@ def sound_notification(frequency: int, duration: int):
 
 def is_internet_available(url: str, timeout: int) -> bool:
     # for url in urls:
-    retry_strategy = Retry(total=100, status_forcelist=[429, 500, 502, 503, 504])
-    adapter = HTTPAdapter(max_retries=retry_strategy)
-    http = requests.Session()
-    http.mount("http://", adapter)
-    http.mount("https://", adapter)
     try:
-        response = http.get(url, timeout=timeout)
-        if response.status_code == 200:
+        request = requests.get(url, timeout=timeout)
+        if request.status_code == 200:
             return True
         else:
             return False
@@ -52,7 +45,6 @@ def is_internet_available(url: str, timeout: int) -> bool:
 if __name__ == "__main__":
     if is_internet_available(url, timeout):
         print("Connected to the Internet")
-        # sys.exit(0)
     else:
         print("No Internet connection")
         sound_notification(frequency, duration)
