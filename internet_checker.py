4a611ea0805b3e988c2442020fff12410bf0dcff
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-06-14 01:02:55 +0300

    Fix black & isort

--- internet_checker.py
+++ internet_checker.py
@@ -6,7 +6,6 @@ import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
-
 # Arg parse for param
 # Use URL from args & file
 url = "https://www.google.com"
