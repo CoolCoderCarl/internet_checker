f15819bb056da4b18eb27d1b7862fd1717ec7067
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-06-16 00:12:35 +0300

    Update internet_checker.py

--- internet_checker.py
+++ internet_checker.py
@@ -22,7 +22,7 @@ def internet_available(url="https://www.google.com"):
     try:
         response = requests.get(url, timeout=5)
         if response.status_code == 200:
-            sys.exit(0)
+            exit(0)
         else:
             sound_notification()
     except (requests.ConnectionError, requests.Timeout):
