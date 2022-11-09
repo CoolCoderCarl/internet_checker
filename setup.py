18faf4206bb1db55e609229e5b32090f38a0fbc4
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-11-10 00:31:38 +0300

    Add setup.py & update tests

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="internet_checker",
+    version="0.0.1",
+    packages=["tests", "internet_checker"],
+    url="",
+    license="GNU GPL V3",
+    author="CoolCoderCarl",
+    author_email="",
+    description="",
+)
--- tests/test_internet_checker.py
+++ tests/test_internet_checker.py
@@ -1,10 +1,10 @@
 from internet_checker import internet_checker as i_c
 
-DEFAULT_URL = "https://www.google.com"
+DEFAULT_URL = "http://www.google.com"
 
 
-def test_latency_is(url: str, retry_count: int):
-    isinstance(i_c.latency_is(url, retry_count), float)
+def test_latency_is():
+    return isinstance(i_c.latency_is(DEFAULT_URL, 1), float)
 
 
 if __name__ == "__main__":
